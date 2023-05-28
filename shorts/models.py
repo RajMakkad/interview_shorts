6aeb0405bf5955913d64824a5ca7cb3df870eadc
Raj Makkad <rsmakkad29@gmail.com>
2023-05-29 01:13:08 +0530

    1. Changed the default timezone to indian standard from UTC in the settings.py module. 2. Registered Short model/table inside the admin.py module of the shorts app. 3. Created a new Short model/table inside the models.py module of shorts app. 4. Added more fields inside the home.html template. 5. Configured the home function of shorts/views.py module to take data directly from the database instead of dummy data.

--- shorts/models.py
+++ shorts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# imported User from auth model for author field.
+class Short(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    experience = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    difficulty = models.CharField(max_length=100)
+    branch = models.CharField(max_length=100)
+    
+    # This function is used to specify how the table rows are gonna look from the outside.
+    def __str__(self):
+        return self.title
--- shorts/views.py
+++ shorts/views.py
@@ -1,25 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Short
 
-shorts = [
-    {
-        'name': 'Raj Makkad',
-        'time': '25May2023',
-        'title': 'HackerEarth | Offcampus-Intern  | Interview Experience',
-        'exp': '1 OA + 1 Tech + 1 Behaviour + 1 Hr'
-    },
-    {
-        'name': 'Raj Makkad',
-        'time': '26May2023',
-        'title': 'Zscaler | Intern + FTE | Interview Experience',
-        'exp': '1 OA + 3 Tech + 1 Hr'
-    }
-]
 
-# In render content is sent as a dictionary, we can directly send a dictionary as a the third argument.
 def home(request):
-    data = {
-        'shorts' : shorts,
-        'title' : 'Home'
-    }
-    return render(request, 'shorts/home.html', data)
+    return render(request, 'shorts/home.html', {'shorts' : Short.objects.all(), 'title' : 'Home'})
