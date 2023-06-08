9d4273d3b33d003c5c9dfd765e6014a61cb32dfd
RajMakkad <rsmakkad29@gmail.com>
2023-06-09 00:45:37 +0530

    1. Added functionality to open each short from home.

--- shorts/views.py
+++ shorts/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Short
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    return render(request, 'shorts/home.html', {'shorts' : Short.objects.all(), 'title' : 'Home'})
+    short = Short.objects.all()
+    short = short.order_by('post_date').reverse()
+    if not request.user.is_authenticated:
+        messages.info(request, 'Login to see Wonders.')
+    return render(request, 'shorts/home.html', {'shorts' : short, 'title' : 'Home'})
+
+
+@login_required
+def detailedShorts(request, short_id):
+    try:
+        short = Short.objects.get(id=short_id)
+        return render(request, 'shorts/short.html', {'short' : short, 'title' : 'Short'})
+    except:
+        return render(request, 'shorts/i_exists.html', {'title' : 'Bitch!!'})
